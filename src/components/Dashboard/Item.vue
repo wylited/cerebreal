a0edf0350227758b1332cd9e66a7a7096d676a27
wylited <wylited@gmail.com>
2022-11-02 13:02:47 +0800

    add files

---
+++ src/components/Dashboard/Item.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+interface Props {
+  item: string
+  tooltip: string
+  link?: string
+}
+
+const {tooltip, item} = defineProps<Props>()
+</script>
+
+<template>
+    <div class="tooltip tooltip-right" :data-tip="tooltip">
+        <a class="btn btn-ghost"> 
+            <div class="flex flex-row align-middle">
+                <slot></slot>
+                <p class="pl-5"> {{item}} </p>
+            </div>
+        </a>
+    </div>
+</template>
